781af7e658244334431da1f1f6bae373a86135ed
hammaadkk <151374850+hammaadkk@users.noreply.github.com>
2024-01-31 14:23:47 +0000

    test2

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,10 @@
-console.log("welcome to js")
+var Name = "ali";
+const accountId = 36749;
+let city = "mardan";
+accountState="jaipur";
+let testingnotdefined ="";
+
+// accountId = 03934;
+// console.log(accountId)
+
+console.table([Name, accountId, city, accountState,testingnotdefined])

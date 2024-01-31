211f24dbd04df748b0966b36e471ec873f96aaa8
hammaadkk <151374850+hammaadkk@users.noreply.github.com>
2024-01-31 20:55:19 +0000

    vs code local testing of commit

---
+++ basics/variable01.js
@@ -0,0 +1,12 @@
+var Name = "abu bakar";
+let AccountType= "Current account";
+const AccountNo = 336784;
+AccoutCity = "jaipur";
+
+let AccountHolder;
+
+// AccountNo = 85405;
+
+console.log("AccountNO");
+
+console.table([Name, AccountType, AccountNo, AccoutCity, AccountHolder ])

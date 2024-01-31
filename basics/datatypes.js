a6fe55ab131eef1e1987603441e9207f99474a08
hammaadkk <151374850+hammaadkk@users.noreply.github.com>
2024-01-31 16:22:47 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,16 @@
+"anything"; //treat all code as the newer version of js
+
+//alert(3 +3); //we cannot use this in nodejs 
+
+//string var name= "ahmad";
+//boolean true, false,
+//bigint
+// Number
+//undefined
+
+//remember Null is and object and undefined is datatype
+
+let state = null;
+console.log(typeof state);
+let city;
+console.log(typeof city)

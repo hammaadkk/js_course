58477cc959e691e69593aa47b5a9112db8142683
hammaadkk <151374850+hammaadkk@users.noreply.github.com>
2024-02-02 15:58:57 +0000

    Datatype Conversion

---
+++ basics/conversionOp.js
@@ -0,0 +1,13 @@
+let score = "454abc";
+
+let ValueInNumber = Number(score);
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+
+
+// "324" this can convert into number
+// "328bb" this cannot convert to number to number it wil give NaN
+// true this will give 1
+// false this will give 0
+// Null this will give 0
+// Undefined cannot convert, will give NaN

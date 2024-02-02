e25e52feaed6af4219c94d5a7ee0337d9b18c1c9
hammaadkk <151374850+hammaadkk@users.noreply.github.com>
2024-02-02 17:22:11 +0000

    Conversion Done

--- basics/conversionOp.js
+++ basics/conversionOp.js
@@ -1,13 +1,40 @@
-let score = "454abc";
+// let score = "454abc";
 
-let ValueInNumber = Number(score);
-console.log(typeof ValueInNumber);
-console.log(ValueInNumber);
+// let ValueInNumber = Number(score);
+// console.log(typeof ValueInNumber);
+// console.log(ValueInNumber);
 
 
+// Number
 // "324" this can convert into number
 // "328bb" this cannot convert to number to number it wil give NaN
 // true this will give 1
 // false this will give 0
 // Null this will give 0
 // Undefined cannot convert, will give NaN
+// "test" cannot convert, will give NaN
+
+let booleanCheck = "as";
+
+let booleanConvert = Boolean(booleanCheck);
+
+console.log(booleanConvert);
+console.log(booleanCheck);
+
+// Boolean
+// 1 will give true
+// 0 will give false
+// "" will give false
+// null will false 
+// undefined will give false
+// 32423 will give true
+// -213 will give true
+// "23492" will give true
+// "sdhk" will give true
+
+// Number to String 
+
+let anyNumber = 33;
+
+let nbrToString = String(anyNumber);
+console.log(typeof nbrToString);

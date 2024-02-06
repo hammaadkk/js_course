d2a8431c6c554cc9894aed8dc94b16665a79716c
hammaadkk <151374850+hammaadkk@users.noreply.github.com>
2024-02-06 16:11:31 +0000

    Comparison

---
+++ basics/comparison.js
@@ -0,0 +1,12 @@
+console.log("23d"> 1);
+
+// remember always compare same datatype
+// console.log(null >0);
+// console.log(null <0);
+// console.log(null ==0);
+// console.log(null >=0); (true) //the reason is that an equality == and comparison><>=<= work differently. Comparison convert null to a number, treating it as 0 thats why null >=0 and true and null >0 is false
+
+
+// double equal (==) and triple equal (===)
+
+// double equal just check values and triple equal check value as well as datatype

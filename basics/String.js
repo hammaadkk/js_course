4c88205c320daa4b8de1b82aeb3de738eced89fb
hammaadkk <151374850+hammaadkk@users.noreply.github.com>
2024-02-13 21:32:48 +0000

    string done object start

---
+++ basics/String.js
@@ -0,0 +1,26 @@
+// const Name = "ahmad";
+// const LastName = " ali";
+
+// console.log(Name + LastName);
+
+// console.log(`My Name is ${Name}${LastName}`);
+
+// we can declare string variable in different way
+
+let myName = new String("Hammaad")
+
+console.log(myName);
+
+// index check
+
+console.log(myName[3]);
+// length check
+
+console.log(myName.length);
+ 
+// to upper case
+console.log(myName.toUpperCase());
+
+const SpaceName = new String("       hammaad     ");
+console.log(SpaceName);
+console.log(SpaceName.trim());

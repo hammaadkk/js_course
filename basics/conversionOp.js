71c2787954ed1ecd73f4298f7597e1e8d5cbb876
hammaadkk <151374850+hammaadkk@users.noreply.github.com>
2024-03-13 18:50:02 +0000

    updated

--- basics/conversionOp.js
+++ basics/conversionOp.js
@@ -6,68 +6,68 @@
 // console.log(typeof ValueInNumber);
 // console.log(ValueInNumber);
 
-// "324" this can convert into number
-// "328bb" this cannot convert to number to number it wil give NaN
-// true this will give 1
-// false this will give 0
-// Null this will give 0
-// Undefined cannot convert, will give NaN
-// "test" cannot convert, will give NaN
-// ***************************************************
+// // "324" this can convert into number
+// // "328bb" this cannot convert to number to number it wil give NaN
+// // true this will give 1
+// // false this will give 0
+// // Null this will give 0
+// // Undefined cannot convert, will give NaN
+// // "test" cannot convert, will give NaN
+// // ***************************************************
 
-// Number, String to Boolean
-let booleanCheck = "as";
+// // Number, String to Boolean
+// let booleanCheck = "as";
 
-let booleanConvert = Boolean(booleanCheck);
+// let booleanConvert = Boolean(booleanCheck);
 
-console.log(booleanConvert);
-console.log(booleanCheck);
+// console.log(booleanConvert);
+// console.log(booleanCheck);
 
 
-// 1 will give true
-// 0 will give false
-// "" will give false
-// null will false 
-// undefined will give false
-// 32423 will give true
-// -213 will give true
-// "23492" will give true
-// "sdhk" will give true
-// ********************************************
-// Number to String 
+// // 1 will give true
+// // 0 will give false
+// // "" will give false
+// // null will false 
+// // undefined will give false
+// // 32423 will give true
+// // -213 will give true
+// // "23492" will give true
+// // "sdhk" will give true
+// // ********************************************
+// // Number to String 
 
-let anyNumber = 33;
+// let anyNumber = 33;
 
-let nbrToString = String(anyNumber);
-console.log(typeof nbrToString);
+// let nbrToString = String(anyNumber);
+// console.log(typeof nbrToString);
 
-// Operators
-// console.log(2+2)
-// console.log(2-2)
-// console.log(2*2)
-// console.log(2/2)
-// console.log(2%3)
-// console.log(2**2) 2 to the power of 2
-// ******************************************
+// // Operators
+// // console.log(2+2)
+// // console.log(2-2)
+// // console.log(2*2)
+// // console.log(2/2)
+// // console.log(2%3)
+// // console.log(2**2) 2 to the power of 2
+// // ******************************************
 
-// postive value to negative
+// // postive value to negative
 
-let PosValue = 3;
-let NegTheValue = -PosValue
-console.log(NegTheValue);
+// let PosValue = 3;
+// let NegTheValue = -PosValue
+// console.log(NegTheValue);
 
-let FirstName = "Abid";
-let LastName = " Ali"
+// let FirstName = "Abid";
+// let LastName = " Ali"
 
-let FullName = FirstName + LastName
+// let FullName = FirstName + LastName
 
-console.log(FullName);
+// console.log(FullName);
 
-// ******************************************
+// // ******************************************
 
-// Confusing Code
-console.log(2 + "2"); //result 22
-console.log(2+2 + "3"); //result 43
-console.log("2" + 3+2) //result 232
+// // Confusing Code
+// console.log(2 + "2"); //result 22
+// console.log(2+2 + "3"); //result 43
+// console.log("2" + 3+2) //result 232
 
-// ******************************************
+// // ******************************************
